d82323798097893dd3172fd90c17ffb255432304
Rob K <kaz.brah@gmail.com>
2020-05-10 18:41:50 -0400

    code cleanup

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,15 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 1
     var dice = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        rightDiceNumber = Int.random(in: 0...5)
-        leftDiceNumber = Int.random(in: 0...5)
-        diceImageView1.image = dice[leftDiceNumber]
-        diceImageView2.image = dice[rightDiceNumber]
+        diceImageView1.image = dice[Int.random(in: 0...5)]
+        diceImageView2.image = dice[Int.random(in: 0...5)]
     }
 }
 
