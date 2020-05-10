2aaf8abe830d965e071d6cb5c4b2496220b4af10
Rob K <kaz.brah@gmail.com>
2020-05-10 18:36:08 -0400

    Random dice rolling application

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -2,19 +2,25 @@
 //  ViewController.swift
 //  Dicee-iOS13
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Robert Kazirut.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    // IBOutlet allows to references a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 1
+    var dice = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        rightDiceNumber = Int.random(in: 0...5)
+        leftDiceNumber = Int.random(in: 0...5)
+        diceImageView1.image = dice[leftDiceNumber]
+        diceImageView2.image = dice[rightDiceNumber]
     }
-
-
 }
 
